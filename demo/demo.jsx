129553d626696e5211e946d49cfdcbae21942166
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2015-07-30 10:49:26 +0800

    init

---
+++ demo/demo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Component from '../src/index';
+
+var config = {};
+
+React.render(
+  <Component {...config}/>,
+  document.getElementById('demo')
+);
---
+++ src/lib/Component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Component extends React.Component {
+	constructor() {
+		super();
+	}
+	render() {
+		return (
+			<div>Hello React!</div>
+		);
+	}
+}
